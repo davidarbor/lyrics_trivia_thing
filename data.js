249f7bdd92df2d76ad81616cddc204579d18c538
David Arbor <davidarbor@students.berkeley.net>
2018-03-07 10:33:19 -0800

    stuff

--- data.js
+++ data.js
@@ -1,101 +1,120 @@
 var song1={
     songName:"All the Stars",
     songArtist:"Kendrick Lamar & SZA",
-    snippet:"This may be the night that my dreams might let me know"
+    snippet:"This may be the night that my dreams might let me know",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview128/v4/69/8c/bc/698cbc2e-9d36-2143-377b-3a8bbf86bd16/mzaf_7018013992683721595.plus.aac.p.m4a"
 };
 var song2={
     songName:"Titanium",
     songArtist:"David Guetta feat. Sia",
-    snippet:"You shoot me down, but I won't fall"
+    snippet:"You shoot me down, but I won't fall",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music4/v4/36/a8/fd/36a8fd1d-25e6-ece5-9bc2-0b6348401646/mzaf_2199857968142882008.plus.aac.p.m4a"
 };
 var song3={
     songName:"So Sick",
     songArtist:"Ne-Yo",
-    snippet:"Said I'm so sick of love songs"
+    snippet:"No more walking around with my head down",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/2a/8b/ea/mzm.lzvxrrds.aac.p.m4a"
 };
 var song4={
     songName:"Over",
     songArtist:"Drake",
-    snippet:"Y'all know them? Well, fuck it, me either"
+    snippet:"Y'all know them? Well, fuck it, me either",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/ea/0b/9e/mzm.uqquecmw.aac.p.m4a"
 };
 var song5={
     songName:"Alright",
     songArtist:"Kendrick Lamar",
-    snippet:"I cross my heart and hope to die"
+    snippet:"I cross my heart and hope to die",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music3/v4/40/6c/4d/406c4d92-5858-698d-ab2b-0d549145c41b/mzaf_2175756654202971871.plus.aac.p.m4a"
 };
 var song6={
     songName:"Seasons",
     songArtist:"Mozzy, Sjava, & Reason",
-    snippet:"Trapped in the system, trafficking drugs"
+    snippet:"Trapped in the system, trafficking drugs",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview128/v4/7f/6b/18/7f6b18b7-7a8a-93f1-3478-e99f74a10cef/mzaf_1861454364091027439.plus.aac.p.m4a"
 };
 var song7={
     songName:"Empire State of Mind",
     songArtist:"Jay-Z & Alicia Keys",
-    snippet:"Concrete jungle where dreams are made of"
+    snippet:"Concrete jungle where dreams are made of",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/b5/f6/aa/mzm.eyxpzlfg.aac.p.m4a"
 };
 var song8={
     songName:"m.A.A.d City",
     songArtist:"Kendrick Lamar feat. MC Eiht",
-    snippet:"Go buy a chopper and have a doctor on speed dial, I guess"
+    snippet:"Go buy a chopper and have a doctor on speed dial, I guess",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/v4/5c/07/82/5c078219-ebbd-2428-bb9a-6a5d5c8f218f/mzaf_6232676577172167520.plus.aac.p.m4a"
 };
 var song9={
     songName:"Finesse (Remix)",
     songArtist:"Bruno Mars feat. Cardi B",
-    snippet:"When I'm walkin' with you, I watch the whole room change"
+    snippet:"When I'm walkin' with you, I watch the whole room change",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview118/v4/7d/ba/3f/7dba3f7f-1ef0-9ec7-302b-1e828bd52984/mzaf_2045576369423603849.plus.aac.p.m4a"
 };
 var song10={
     songName:"Lose Yourself",
     songArtist:"Eminem",
-    snippet:"Coast to coast shows, he's known as the globetrotter"
+    snippet:"Coast to coast shows, he's known as the globetrotter",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/v4/f1/7b/d6/f17bd6e3-55c0-b7e0-9863-bc522900e950/mzaf_3789655364834981580.plus.aac.p.m4a"
 };
 var song11={
     songName:"No Church in the Wild",
     songArtist:"Kanye West feat. Jay-Z and Frank Ocean",
-    snippet:"But we prolly spend it all 'cause the pain ain't cheap"
+    snippet:"But we prolly spend it all 'cause the pain ain't cheap",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/v4/79/16/94/7916944e-efcd-b2ad-1504-2f8e70b3916c/mzaf_4497442509487219435.plus.aac.p.m4a"
 };
 var song12={
     songName:"DUCKWORTH.",
     songArtist:"Kendrick Lamar",
-    snippet:"It was always me vs the world"
+    snippet:"It was always me vs the world",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview122/v4/d1/a8/06/d1a8063e-5c26-e028-d8a1-e13c0871d9a1/mzaf_2104777618282498511.plus.aac.p.m4a"
 };
 var song13={
     songName:"New York, New York",
     songArtist:"Frank Sinatra",
-    snippet:"These vagabond shoes are longing to stray"
+    snippet:"These vagabond shoes are longing to stray",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview71/v4/4f/32/fc/4f32fce8-4a34-77aa-e653-584d4d9e786a/mzaf_151095894833543861.plus.aac.p.m4a"
 };
 var song14={
     songName:"Payphone",
     songArtist:"Maroon 5 feat. Wiz Khalifa",
-    snippet:"But even the sun sets in paradise"
+    snippet:"But even the sun sets in paradise",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview71/v4/bb/db/61/bbdb615c-903c-2a93-3e0a-a58b1f560f75/mzaf_7874722204857162894.plus.aac.p.m4a"
 };
 var song15={
     songName:"Can't Stop the Feeling",
     songArtist:"Justin Timberlake",
-    snippet:"I got that sunshine in my pocket"
+    snippet:"I got that sunshine in my pocket",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview62/v4/1b/72/fc/1b72fc2f-bca9-cb31-5a5d-9fce163fdfda/mzaf_2671500077676982988.plus.aac.p.m4a"
 };
 var song16={
-    songName:"Furthest Thing",
-    songArtist:"Drake",
-    snippet:"Promise to break everybody off before I break down"
+    songName:"Take Care",
+    songArtist:"Drake feat. Rihanna",
+    snippet:"My mind didn't change and I still feel the same",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/55/a5/07/mzm.icpbukrf.aac.p.m4a"
 };
 var song17={
     songName:"Where Is the Love?",
     songArtist:"Black Eyed Peas",
-    snippet:"It just ain't the same, old ways have changed"
+    snippet:"It just ain't the same, old ways have changed",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/2d/a0/9c/mzm.xcohlztr.aac.p.m4a"
 };
 var song18={
     songName:"Not Afraid",
     songArtist:"Eminem",
-    snippet:"I shoot for the moon, but I'm too busy gazing at stars"
+    snippet:"I shoot for the moon, but I'm too busy gazing at stars",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/d9/f9/ba/mzm.ohfuogtw.aac.p.m4a"
 };
 var song19={
     songName:"No Problem",
     songArtist:"Chance the Rapper feat. Lil Wayne & 2 Chainz",
-    snippet:"Milly rockin', scoopin' all the blessings out my lap"
+    snippet:"Milly rockin', scoopin' all the blessings out my lap",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview20/v4/84/7b/30/847b305e-75dd-60b1-2915-b45c53b85871/mzaf_912958891308828755.plus.aac.p.m4a"
 };
 var song20={
     songName:"Bloody Waters",
     songArtist:"Ab-Soul feat. Anderson .Paak & James Blake",
-    snippet:"Just keep a dock on standby, charter a boat"
-    //audioSnippetUrl: ""
+    snippet:"Just keep a dock on standby, charter a boat",
+    songURL:"https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview118/v4/a0/b8/f1/a0b8f1eb-ae45-eb38-918a-7036d897e7ac/mzaf_5053714506105984171.plus.aac.p.m4a"
 };
--- script.js
+++ script.js
@@ -5,6 +5,27 @@
 $(document).ready(function(){
     var $submit = $("#startButton");
     var $output = $("#lyricsOutput");
+
+    songNum=Math.floor(Math.random()*songsArray.length);
+    songNum2=Math.floor(Math.random()*songsArray.length);
+    songNum3=Math.floor(Math.random()*songsArray.length);
+    songNum4=Math.floor(Math.random()*songsArray.length);
+
+    currentSong=songsArray[songNum];
+
+    button1=document.getElementById("firstOption");
+    button2=document.getElementById("secondOption");
+    button3=document.getElementById("thirdOption");
+    button4=document.getElementById("fourthOption");
+    buttonArray=[button1, button2, button3, button4];
+
+    buttonOption=Math.floor(Math.random()*buttonArray.length);
+
+    /*text1=document.getElementById("firstOption").textContent;
+    text2=document.getElementById("secondOption").textContent;
+    text3=document.getElementById("thirdOption").textContent;
+    text4=document.getElementById("fourthOption").textContent;*/
+
     /*$submit.click(function(){
         $output.empty();
         // $.ajax({
@@ -30,68 +51,136 @@ $(document).ready(function(){
         });
 
     });*/
+    console.log(songNum);
+    console.log(songNum2);
+    console.log(songNum3);
+    console.log(songNum4);
+    console.log(buttonOption);
 });
 
-var songsArray = [song1, song2, song3, song4, song5, song6, song7, song8, song9, song10, song11, song12, song13, song14, song15, song16, song17, song18, song19, song20];
+function resetData(){
+    songNum=Math.floor(Math.random()*songsArray.length);
+    songNum2=Math.floor(Math.random()*songsArray.length);
+    songNum3=Math.floor(Math.random()*songsArray.length);
+    songNum4=Math.floor(Math.random()*songsArray.length);
 
-var songNum=Math.floor(Math.random()*songsArray.length);
-var songNum2=Math.floor(Math.random()*songsArray.length);
-var songNum3=Math.floor(Math.random()*songsArray.length);
-var songNum4=Math.floor(Math.random()*songsArray.length);
+    currentSong=songsArray[songNum];
 
-var currentSong=songsArray[songNum];
+    button1=document.getElementById("firstOption");
+    button2=document.getElementById("secondOption");
+    button3=document.getElementById("thirdOption");
+    button4=document.getElementById("fourthOption");
+    buttonOption=Math.floor(Math.random()*buttonArray.length);
 
-var button1=document.getElementById("firstOption");
-var button2=document.getElementById("secondOption");
-var button3=document.getElementById("thirdOption");
-var button4=document.getElementById("fourthOption");
-var buttonArray=[button1, button2, button3, button4];
+    text1=document.getElementById("firstOption").textContent;
+    text2=document.getElementById("secondOption").textContent;
+    text3=document.getElementById("thirdOption").textContent;
+    text4=document.getElementById("fourthOption").textContent;
+}
 
-var buttonOption=Math.floor(Math.random()*buttonArray.length);
+songsArray=[song1, song2, song3, song4, song5, song6, song7, song8, song9, song10, song11, song12, song13, song14, song15, song16, song17, song18, song19, song20];
 
 function gameStart(){
+    resetData();
     $("#startButton").fadeOut(200);
     $("body").pagecontainer("change", "#page2", {});
+    console.log(button1);
     document.getElementById("lyricsOutput").innerHTML=currentSong.snippet;
     switch(buttonOption){
         case 0:
             document.getElementById("firstOption").textContent=currentSong.songName;
+            document.getElementById("secondOption").textContent=songsArray[songNum2].songName;
+            document.getElementById("thirdOption").textContent=songsArray[songNum3].songName;
+            document.getElementById("fourthOption").textContent=songsArray[songNum4].songName;
             break;
         case 1:
+            document.getElementById("firstOption").textContent=songsArray[songNum2].songName;
             document.getElementById("secondOption").textContent=currentSong.songName;
+            document.getElementById("thirdOption").textContent=songsArray[songNum3].songName;
+            document.getElementById("fourthOption").textContent=songsArray[songNum4].songName;
             break;
         case 2:
+            document.getElementById("firstOption").textContent=songsArray[songNum2].songName;
+            document.getElementById("secondOption").textContent=songsArray[songNum3].songName;
             document.getElementById("thirdOption").textContent=currentSong.songName;
+            document.getElementById("fourthOption").textContent=songsArray[songNum4].songName;
             break;
         case 3:
+            document.getElementById("firstOption").textContent=songsArray[songNum2].songName;
+            document.getElementById("secondOption").textContent=songsArray[songNum3].songName;
+            document.getElementById("thirdOption").textContent=songsArray[songNum4].songName;
             document.getElementById("fourthOption").textContent=currentSong.songName;
             break;
     }
+    text1=document.getElementById("firstOption").textContent;
+    text2=document.getElementById("secondOption").textContent;
+    text3=document.getElementById("thirdOption").textContent;
+    text4=document.getElementById("fourthOption").textContent;
+    console.log(text1);
 }
 
 function guessFirst(){
-    $("body").pagecontainer("change", "#page3", {});
+    $("#audio1").attr("src",currentSong.songURL);
+    $("#audio2").attr("src",currentSong.songURL);
+    document.getElementById("songName1").innerHTML=currentSong.songName;
+    document.getElementById("songArtists1").innerHTML=currentSong.songArtist;
+    document.getElementById("songName2").innerHTML=currentSong.songName;
+    document.getElementById("songArtists2").innerHTML=currentSong.songArtist;
+    if(text1==currentSong.songName){
+        $("body").pagecontainer("change", "#page3", {});
+    }
+    else{
+        $("body").pagecontainer("change", "#page4", {});
+    }
 }
 
 function guessSecond(){
-    $("body").pagecontainer("change", "#page4", {});
+    $("#audio1").attr("src",currentSong.songURL);
+    $("#audio2").attr("src",currentSong.songURL);
+    document.getElementById("songName1").innerHTML=currentSong.songName;
+    document.getElementById("songArtists1").innerHTML=currentSong.songArtist;
+    document.getElementById("songName2").innerHTML=currentSong.songName;
+    document.getElementById("songArtists2").innerHTML=currentSong.songArtist;
+    if(text2==currentSong.songName){
+        $("body").pagecontainer("change", "#page3", {});
+    }
+    else{
+        $("body").pagecontainer("change", "#page4", {});
+    }
+
 }
 
 function guessThird(){
-    $("body").pagecontainer("change", "#page4", {});
+    $("#audio1").attr("src",currentSong.songURL);
+    $("#audio2").attr("src",currentSong.songURL);
+    document.getElementById("songName1").innerHTML=currentSong.songName;
+    document.getElementById("songArtists1").innerHTML=currentSong.songArtist;
+    document.getElementById("songName2").innerHTML=currentSong.songName;
+    document.getElementById("songArtists2").innerHTML=currentSong.songArtist;
+    if(text3==currentSong.songName){
+        $("body").pagecontainer("change", "#page3", {});
+    }
+    else{
+        $("body").pagecontainer("change", "#page4", {});
+    }
 }
 
 function guessFourth(){
-    $("body").pagecontainer("change", "#page4", {});
+    $("#audio1").attr("src",currentSong.songURL);
+    $("#audio2").attr("src",currentSong.songURL);
+    document.getElementById("songName1").innerHTML=currentSong.songName;
+    document.getElementById("songArtists1").innerHTML=currentSong.songArtist;
+    document.getElementById("songName2").innerHTML=currentSong.songName;
+    document.getElementById("songArtists2").innerHTML=currentSong.songArtist;
+    if(text4==currentSong.songName){
+        $("body").pagecontainer("change", "#page3", {});
+    }
+    else{
+        $("body").pagecontainer("change", "#page4", {});
+    }
 }
 
 function backToSecond(){
     $("body").pagecontainer("change", "#page2", {});
     gameStart();
 }
-
-console.log(songNum);
-console.log(songNum2);
-console.log(songNum3);
-console.log(songNum4);
-console.log(buttonOption);
